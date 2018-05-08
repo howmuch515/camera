2ee593c7710ca7e0a1f449d240dec18ca347c4c1
appbana <appbana@dot1x296.14-31-23.wlan.ms.dendai.ac.jp>
2018-05-08 16:04:50 +0900

    [fix] destroy 3 wake-up-pictures

--- camera.py
+++ camera.py
@@ -2,17 +2,19 @@ import cv2, argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Take a picture.")
-    parser.add_argument('file_name', metavar="FILE_NAME",
-                        help="picture's name")
+    parser.add_argument('file_name', metavar="FILE_NAME", help="picture's name")
+    parser.add_argument('-n', '--file_name', help="picture's name")
     parser.add_argument('-v', '--version', action="version", version="Ver.0.0")
 
     args = parser.parse_args()
+
     FILE_NAME = args.file_name
 
     # init
-    FILE_PATH = "/tmp/" + FILE_NAME
+    FILE_PATH = "/tmp/" + FILE_NAME + ".jpg"
     cap = cv2.VideoCapture(0)
-    cap.read() # 1枚目は真っ暗な写真になるので捨てる
+    for i in range(3):
+        cap.read() # 初期の写真はカメラが起動して間もないため暗いので、捨てる
 
     # main
     ret, im = cap.read()
